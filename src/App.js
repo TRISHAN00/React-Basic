9878ba0ad49ae356b613d5d07565f6f9f82eb07a
TRISHAN00 <trishansaha43@gmail.com>
2022-02-20 12:29:13 +0600

    pass function using props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Profile from './components/profile';
+
+class Child extends Component {
+  render() {
+    this.props.func(this);
+    return <h1>I am Child Component</h1>
+  }
+}
 
 class App extends Component {
+  getContext(context) {
+    console.log(context);
+  }
   render() {
+    this.getContext(this)
     return (
       <div>
-        <Profile/>
+        <h1>Pass Function as Props</h1>
+        <Child func={this.getContext} />
       </div>
     );
   }
