2dd6f3cb6add9b20c4d27be9aa4d93241349fd00
TRISHAN00 <trishansaha43@gmail.com>
2022-02-24 04:22:05 +0600

    onsubmit handler

--- src/components/anotherForm/uncontrolled-form.js
+++ src/components/anotherForm/uncontrolled-form.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class UncontrolledForm extends Component {
+   
+    handleOnSubmit = e => {
+        e.preventDefault()
+        const data = {
+            name : e.target.name.value,
+            email : e.target.email.value,
+            password : e.target.password.value,
+        }
+        console.log(data);
+    }
+     
     render() {
         return (
             <div className='container'>
                 <h2>Basic Form</h2>
-                <form className='form-group'>
+                <form onSubmit={this.handleOnSubmit} >
                     <input className='form-control' type="text" name='name' placeholder='name' />
                     <input className='form-control' type="text" name='email' placeholder='email' />
                     <input className='form-control' type="text" name='password' placeholder='password' />
-                    <input className='form-control' type="submit" value="Submit" />
+                    <br />
+                    <input className='form-control btn btn-success' type="submit" value="Submit" />
                 </form>
             </div>
         );
