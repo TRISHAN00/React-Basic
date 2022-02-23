e64c2a1b50c39e8a25250e6eaba07032dfaa71b6
TRISHAN00 <trishansaha43@gmail.com>
2022-02-23 22:15:55 +0600

    design UI of form

--- src/components/Form/Index.js
+++ src/components/Form/Index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import './Index.module.css';
 
 class Form extends Component {
+    
     render() {
+        
         return (
             <div className='container'>
                 <h2>Submit Your Data...</h2>
+                <form>
+                    <h3>First Name</h3>
+                    <input type="text" />
+                    <h3>Last Name</h3>
+                    <input type="text" />
+                    <h3>Password</h3>
+                    <input type="password" />
+                    <h3>Country</h3>
+                    <select>
+                        <option>Select Country</option>
+                        <option value="Bangladesh">Bangladesh</option>
+                        <option value="India">India</option>
+                        <option value="Pakistan">Pakistan</option>
+                        <option value="London">London</option>
+                        <option value="China">China</option>
+                    </select>
+                    <br/>
+                    <br/>
+                    <input type="submit" />
+                </form>
             </div>
         );
     }
